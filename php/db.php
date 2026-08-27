647f0a5c0bd84a4ab070226104dd04d9fc432a5c
Hemadharshan <dhachu7735@gmail.com>
2026-08-27 11:40:33 +0530

    Add files via upload

--- php/db.php
+++ php/db.php
@@ -17,7 +17,7 @@ $DB_HOST = getenv("DB_HOST") ?: "127.0.0.1";
 $DB_PORT = (int)(getenv("DB_PORT") ?: 3306);
 $DB_NAME = getenv("DB_NAME") ?: "liveconnect";
 $DB_USER = getenv("DB_USER") ?: "root";
-$DB_PASS = getenv("DB_PASS") ?: "JewnPylxQjuZdgJXNGiNaKZoZOPqzCbv";
+$DB_PASS = getenv("DB_PASS") ?: $DB_PASS = getenv("DB_PASS") ?: "";;
 
 
 function get_db_connection()
